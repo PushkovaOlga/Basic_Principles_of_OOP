27467e3bf9b79b8bba3e605259a00c67c8c4a71b
TatianaMaksakova <tatzianar@gmail.com>
2023-10-11 16:21:21 +0300

    add: add class Robocat and sub class

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+class RoboCat():
+    def __init__(self, material, ears, hp):
+        self.material = material
+        self.ears = True
+        self.hp = 9
 
+    def meow(self):
+        print("Мяу")
+
+class GreenRobocat(RoboCat):
+    def __init__(self, material, ears, superpower):
+        super().__init__(material, ears)
+        self.superpower = superpower 
