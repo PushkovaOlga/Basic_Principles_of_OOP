3e189e56ae0d6394d3ebf4b71c0d80fc6b0154ad
TatianaMaksakova <tatzianar@gmail.com>
2023-10-11 16:21:15 +0300

    add: add class Drugs and sub class

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+class Drugs():
+    def __init__(self, properties, strength, dead_chance):
+        self.properties = properties
+        self.strenght = True
+        self.dead_chance = 50
 
+    def MDMA(self):
+        print("MDMA")
