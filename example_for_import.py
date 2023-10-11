1c8bd3a87a98fc994708bfbfa524305af0d00fe5
TatianaMaksakova <tatzianar@gmail.com>
2023-10-11 16:28:59 +0300

    add; add class robocat

---
+++ example_for_import.py
@@ -0,0 +1,13 @@
+class Robocat():
+    def __init__(self, material, ears, hp):
+        self.material = material
+        self.ears = True
+        self.hp = 9
+
+     daf meow(self):
+         print('мяу')
+
+    robocat1 = robocat("metal", True< "9")
+print(robocat1.material)
+def __init__(self, material, ears)
+
