bb2937bb4e25103a6c6476ef2f3662f64c41ab4d
Game_Shakira <103584080+GameShakira@users.noreply.github.com>
2023-10-11 18:25:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 
+class RoboCat():
+    def __init__(self):
+        self.health = 9
+        self.material = "stall"
+        self.ushi = True
+    
+    def INH(self):
+        print("miau")
+
+class GreenRoboCat(RoboCat):
+    def __init__(self, material, health, ushi, super):
+        super().__init__(material, ushi)
+
+
+cat1 = RoboCat()
+cat2 = GreenRoboCat()
+
+cat1.health = 1
+
+print(cat1.health)
+print(cat2.health)
+cat2.INH()
